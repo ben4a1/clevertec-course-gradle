d91ac8136384125feb642cb622c1f183dd278463
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-10 21:17:03 +0300

    feat: main() method in App class (api module), implementation project :core (for :api)

---
+++ api/src/main/java/ru/clevertec/course/gradle/App.java
@@ -0,0 +1,8 @@
+package ru.clevertec.course.gradle;
+
+public class App {
+    public static void main(String[] args) {
+        boolean allPositiveNumbers = Utils.isAllPositiveNumbers("12", "79");
+        System.out.println(allPositiveNumbers);
+    }
+}
