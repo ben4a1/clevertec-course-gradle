baeb941e35eba70bc09d49f43f4955b4a3f45689
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-12 12:49:32 +0300

    feat: still trying create custom plugin

--- lib/src/main/groovy/CustomPlugin.groovy
+++
@@ -1,9 +0,0 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-
-class CustomPlugin implements Plugin<Project>{
-    @Override
-    void apply(Project project) {
-        println 'CustomPlugin works!'
-    }
-}
